c753cef286de10a598bf0e2d34aeb98bd8c29858
ABIKKA VINCY J <avsrm2005@gmail.com>
2026-07-27 22:07:31 +0530

    Add files via upload

---
+++ 05_problem5.py
@@ -0,0 +1,3 @@
+a = int(input("enter the value of a: "))
+b = int(input("enter the value of b: "))
+print ("the average of these two numbers is ", (a+b)/2 )
---
+++ 06_problem6.py
@@ -0,0 +1,3 @@
+a = int(input ("enter the value of a: "))
+
+print ("the square of the number is ", a**2)
